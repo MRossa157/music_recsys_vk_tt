0cd864ff922e13e44cd4e4c43589b03b82c24816
MRossa <maxim.rossa@mail.ru>
2024-04-25 15:02:01 +0400

    Initial commit + dataset preprocessing funcs

---
+++ app/src/config.py
@@ -0,0 +1,3 @@
+# all path are from ./src folder
+
+DATASET_PATH = "../dataset/"
---
+++ app/src/utils.py
@@ -0,0 +1,34 @@
+import os
+import zipfile
+
+import py7zr
+from tqdm import tqdm  # Импортируем tqdm для отображения прогресса
+
+
+def _zip_extract(path, extract_to):
+    with zipfile.ZipFile(path, 'r') as zfile:
+        zfile.extractall(path=extract_to)
+        for filename in tqdm(zfile.namelist(), desc="Разархивируем .zip"):
+            full_path = os.path.join(extract_to, filename)
+            recursive_extract(full_path, extract_to)
+
+def _7z_extract(path, extract_to):
+    with py7zr.SevenZipFile(path, mode='r') as zfile:
+        zfile.extractall(path=extract_to)
+        for filename in tqdm(zfile.getnames(), desc="Разархивируем .7z"):
+            full_path = os.path.join(extract_to, filename)
+            recursive_extract(full_path, extract_to)
+
+def recursive_extract(zip_path, extract_to):
+    if zipfile.is_zipfile(zip_path):
+        _zip_extract(zip_path, extract_to)
+    elif zip_path.endswith('.7z'):
+        _7z_extract(zip_path, extract_to)
+
+def extract_all(archive_path):
+    # Определяем папку, в которой находится основной архив
+    directory = os.path.dirname(archive_path)
+
+    # Начинаем рекурсивное извлечение с основного архива
+    recursive_extract(archive_path, directory)
+    print("Все архивы успешно извлечены.")
