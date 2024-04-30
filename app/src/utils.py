5bb56fea2e415704cccd0595606c792fadf40ed2
MRossa <maxim.rossa@mail.ru>
2024-04-30 19:21:55 +0400

    Optimized the method of obtaining all users and items

--- app/src/utils.py
+++ app/src/utils.py
@@ -1,22 +1,40 @@
+import logging
+from typing import Tuple
+
 import pandas as pd
+from pandas import DataFrame
 
 
-def get_users_and_items():
-    df_members = pd.read_csv(r"app\dataset\members.csv")
-    df_songs = pd.read_csv(r"app\dataset\songs.csv")
+def get_users_and_items(dfs: list[DataFrame]) -> Tuple[list]:
+    """
+    Method that takes as input DataFrames with columns msno, song_id and returns a list of all unique users(msno) and songs(song_id)
 
-    df_test = pd.read_csv(r"app\dataset\test.csv")
-    df_train = pd.read_csv(r"app\dataset\train.csv")
+    Params:
+
+    dfs (List (Pandas Dataframe)) - List of Pandas DataFrames
+    """
+    logging.info('Загружаем данные о песнях и пользователях')
 
     all_users, all_items = [], []
+    for df in dfs:
+        # В некоторых Датафреймах (например df_songs) нет информации о пользователях
+        # поэтому мы просто ловим исключение и ничего с ним не делаем
+        try:
+            all_users.extend(df['msno'].unique().tolist())
+        except KeyError:
+            pass
+        try:
+            all_items.extend(df['song_id'].unique().tolist())
+        except KeyError:
+            pass
 
-    all_users.extend(df_members['msno'].unique().tolist())
-    all_users.extend(df_test['msno'].unique().tolist())
-    all_users.extend(df_train['msno'].unique().tolist())
+    # Удалим дубликаты
+    all_users, all_items = list(set(all_users)), list(set(all_items))
 
-    all_items.extend(df_songs['song_id'].unique().tolist())
-    all_items.extend(df_test['song_id'].unique().tolist())
-    all_items.extend(df_train['song_id'].unique().tolist())
-
-    del df_songs, df_members, df_test, df_train
     return all_users, all_items
+
+
+def get_train_data_ncf():
+    df_train = pd.read_csv(r"app\dataset\train.csv")
+    all_users, all_items = get_users_and_items([df_train])
+    return df_train, all_users, all_items
